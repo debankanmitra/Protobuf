3e6c01e90c4f094b9f48dff6d1f955c0de10389d
debankanmitra <debankanmitra@gmail.com>
2023-06-20 16:08:39 +0530

    commented

--- go_grpc/calculator/server/server.go
+++ go_grpc/calculator/server/server.go
@@ -16,6 +16,15 @@ type server struct {
 }
 
 // RPC response to client
+/*
+*The Calculate method is automatically invoked by the gRPC server when a client makes a request
+*to the server's endpoint for the Calculate RPC method. The gRPC server handles the request and
+*invokes the corresponding method implemented by the server struct, in this case, the Calculate method.
+*The method processes the request, performs the necessary calculations, and returns the response back to the client.
+
+*So, even though the Calculate method is defined outside the main function, it is registered with the gRPC server
+*and automatically called when the server receives a request for the Calculate RPC method.
+ */
 func (*server) Calculate(ctx context.Context, req *pb.SumRequest) (*pb.SumResponse, error) {
 	fmt.Printf("In server Calculate function invoked with %v", req)
 	sum := req.GetNum1() + req.GetNum2()
